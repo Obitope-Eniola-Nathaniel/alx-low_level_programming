0e8019bdde4d8a16f8295419cd762cd9cd1c3b61
Obitope-Eniola-Nathanielgit config --global user.name Obitope-Eniola-Nathaniel <Obitope-Eniola-Nathanielgit config --global user.name Obitope-Eniola-Nathaniel>
2023-09-11 03:26:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
- * Return: 0 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
