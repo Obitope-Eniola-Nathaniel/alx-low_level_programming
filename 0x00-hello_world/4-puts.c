4f5cc77abbe538616d29a4231f6d08848a74a5ee
Obitope-Eniola-Nathanielgit config --global user.name Obitope-Eniola-Nathaniel <Obitope-Eniola-Nathanielgit config --global user.name Obitope-Eniola-Nathaniel>
2023-09-11 02:42:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the entry point A program that print a sentence with put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
